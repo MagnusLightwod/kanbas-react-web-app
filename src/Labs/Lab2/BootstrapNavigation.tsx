2e31506c2217751f3162a4437178fc946b57fdd7
Maggy <chrismegaman@live.com>
2024-09-25 15:46:53 -0400

    update

--- src/Labs/Lab2/BootstrapNavigation.tsx
+++ src/Labs/Lab2/BootstrapNavigation.tsx
@@ -16,6 +16,29 @@ export default function BootstrapNavigation() {
         <a className="nav-link disabled" href="#">Disabled</a>
     </li>
   </ul>
+
+  <div id="wd-css-navigating-with-cards">
+  <h2>Cards</h2>
+  <div className="card"
+       style={{ width: "18rem" }}>
+    <img src="images/stacked.jpg"
+         className="card-img-top" />
+    <div className="card-body">
+      <h5 className="card-title">
+          Stacking Starship
+      </h5>
+      <p className="card-text">
+        Stacking the most powerful rocket in history. Mars or bust!
+      </p>
+      <a href="#" className="btn btn-primary">
+        Boldly Go
+      </a>
+    </div>
+  </div>
 </div>
+
+</div>
+
+
     )
 }
