736d00eb8fd62298f50a1d1ede1dea935934658c
Maggy <chrismegaman@live.com>
2024-09-27 19:34:22 -0400

    update

--- src/Kanbas/Courses/Navigation.tsx
+++ src/Kanbas/Courses/Navigation.tsx
@@ -1,17 +1,51 @@
-import { Link } from "react-router-dom";
+import { Link, useLocation } from "react-router-dom";
+import "../styles.css"; // Assuming your styles are in styles.css
+
+{/* Create an array to store the to links, ids and labels. 
+  Then render those links on the side bar. Where the active is highlighted having black text*/}
 export default function CoursesNavigation() {
-  return (
-    <div id="wd-courses-navigation">
-      <Link id="wd-course-home-link"    to="/Kanbas/Courses/1234/Home">Home</Link><br/>
-      <Link id="wd-course-modules-link" to="/Kanbas/Courses/1234/Modules">Modules
-        </Link><br/>
-      <Link id="wd-course-piazza-link"  to="/Kanbas/Courses/1234/Piazza">Piazza</Link><br/>
-      <Link id="wd-course-zoom-link"    to="/Kanbas/Courses/1234/Zoom">Zoom</Link><br/>
-      <Link id="wd-course-quizzes-link" to="/Kanbas/Courses/1234/Assignments">
-          Assignments</Link><br/>
-      <Link id="wd-course-assignments-link" to="/Kanbas/Courses/1234/Quizzes">Quizzes
-        </Link><br/>
-      <Link id="wd-course-grades-link"  to="/Kanbas/Courses/1234/Grades">Grades</Link><br/>
-      <Link id="wd-course-people-link"  to="/Kanbas/People">People</Link><br/>
+  const location = useLocation();
+
+  const menuLinks = [
+    { to: "/Kanbas/Courses/1234/Home", id: "wd-course-home-link", label: "Home"},
+
+    { to:  "/Kanbas/Courses/1234/Modules", id: "wd-course-modules-link", label:"Modules"},
+
+    { to: "/Kanbas/Courses/1234/Piazza", id: "wd-course-piazza-link", label:"Piazza"},
+
+    { to: "/Kanbas/Courses/1234/Zoom", id: "wd-course-zoom-link", label:"Zoom"},
+
+    { to: "/Kanbas/Courses/1234/Assignments", id: "wd-course-assignments-link", label:"Assignments"},
+
+    { to: "/Kanbas/Courses/1234/Quizzes", id: "wd-course-quizzes-link", label:"Quizzes"},
+
+    { to: "/Kanbas/Courses/1234/People", id: "wd-course-people-link", label:"People"},
+  ];
+
+  return( 
+    <div id="wd-course-naviation" className="wd list-group fs-5 rounded-0">
+      {menuLinks.map((menuLinks) => (
+        <Link
+        
+          key={menuLinks.id} 
+          to={menuLinks.to}  
+          id={menuLinks.id} 
+          className={`list-group-item list-group-item-action border-0 ${
+            location.pathname === menuLinks.to ? "active" : "text-danger"
+          }`}>
+          {menuLinks.label}
+        </Link>
+      ))}
     </div>
-);}
+  );
+}
+
+{/*to= where to link to */}
+{/*${} essentially goes and maps the link to each of the items in the list, then also adds the active and text=danger options for when clicked. Putting this after border-0 means the active or text-danger value is added towards the end */}
+{/* 
+  list-group-item: A Bootstrap class that styles the element as a list group item.
+list-group-item-action: Makes the entire list item clickable and adds hover and active styles.
+border-0: removes border from button
+ location.pathname === menuLinks.to : Checks if path name is same as to: and makes it active
+  {menuLinks.label} render the actual button based on the label
+*/}
