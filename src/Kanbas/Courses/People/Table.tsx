1d8ea48131d3468d21b90f3d5c3f5d0516f1f048
Maggy <chrismegaman@live.com>
2024-09-28 17:33:19 -0400

    update

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -23,7 +23,48 @@ export default function PeopleTable() {
             <td className="wd-last-activity">2020-10-01</td>
             <td className="wd-total-activity">10:21:32</td>
           </tr>
-          {/* Add at least 3 more users such as Bruce Wayne, Steve Rogers, and Natasha Romanoff */}
+            {/* Student 2 */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Christopher</span>{" "}
+              <span className="wd-last-name">Alcantara</span>
+            </td>
+            <td className="wd-login-id">001392071S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">STUDENT</td>
+            <td className="wd-last-activity">2020-10-01</td>
+            <td className="wd-total-activity">10:24:32</td>
+          </tr>
+
+          {/* Student 3 */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Kent</span>{" "}
+              <span className="wd-last-name">Clark</span>
+            </td>
+            <td className="wd-login-id">001837291S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">T.A.</td>
+            <td className="wd-last-activity">2020-10-01</td>
+            <td className="wd-total-activity">10:22:12</td>
+          </tr>
+
+          {/* Student 4 */}
+          <tr>
+            <td className="wd-full-name text-nowrap">
+              <FaUserCircle className="me-2 fs-1 text-secondary" />
+              <span className="wd-first-name">Matthew</span>{" "}
+              <span className="wd-last-name">Patrick</span>
+            </td>
+            <td className="wd-login-id">001392071S</td>
+            <td className="wd-section">S101</td>
+            <td className="wd-role">TEACHER</td>
+            <td className="wd-last-activity">2020-10-2</td>
+            <td className="wd-total-activity">01:30:00</td>
+          </tr>
+          
         </tbody>
       </table>
     </div>
