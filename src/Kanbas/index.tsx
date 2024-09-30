bfd0945238850a194edd709bcbffbde0d2be6dc6
Maggy <chrismegaman@live.com>
2024-09-30 19:21:20 -0400

    update

--- src/Kanbas/index.tsx
+++ src/Kanbas/index.tsx
@@ -4,7 +4,7 @@ import Dashboard from "./Dashboard";
 import Courses from "./Courses";
 import KanbasNavigation from "./Navigation";
 import "bootstrap/dist/css/bootstrap.min.css";
-import "bootstrap/dist/js/bootstrap.bundle.min.js";
+
 import "./styles.css";
 export default function Kanbas() {
     return (
