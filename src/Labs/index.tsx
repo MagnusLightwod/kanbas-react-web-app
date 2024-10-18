03925a4646053240c3e4f5726dbc11e07a5b6f59
Maggy <chrismegaman@live.com>
2024-10-18 18:00:51 -0400

    working lab4 a4

--- src/Labs/index.tsx
+++ src/Labs/index.tsx
@@ -7,9 +7,13 @@ import Lab3 from "./Lab3";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 import Kanbas from "../Kanbas"
+import Lab4 from "./Lab4";
+import store from "./store";
+import { Provider } from "react-redux";
 // Importing Lab1 from lab1 file, defaults to index.tsx
   export default function Labs() {
     return (
+      <Provider store={store}>
       <div id="wd-labs">
         <h2>Christopher Alcantara CS5610 20593</h2>
         
@@ -22,8 +26,10 @@ import Kanbas from "../Kanbas"
         <Route path="Lab1" element={<Lab1 />} />
         <Route path="Lab2" element={<Lab2 />} />
         <Route path="Lab3/*" element={<Lab3 />} />
+        <Route path="Lab4" element={<Lab4/>} />
         <Route path="Kanbas" element ={<Kanbas />} />
       </Routes>
       </div>
+      </Provider>
   );
 }
