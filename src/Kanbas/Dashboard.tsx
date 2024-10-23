14787cc733f3e296ca7bf3ec3893bb8540f0304e
Maggy <chrismegaman@live.com>
2024-10-22 20:04:49 -0400

    working lab4 a4, issue with image on new course

--- src/Kanbas/Dashboard.tsx
+++ src/Kanbas/Dashboard.tsx
@@ -7,13 +7,30 @@ export default function Dashboard() {
   const [course, setCourse] = useState<any>({
     _id: "0", name: "New Course", number: "New Number",
     startDate: "2023-09-10", endDate: "2023-12-15",
-    image: "/images/Lblue.jpg", description: "New Description", 
+    image: "react.jpg", description: "New Description", 
   });
   const addNewCourse = () => {
     const newCourse = { ...course,
                         _id: new Date().getTime().toString() };
     setCourses([...courses, { ...course, ...newCourse }]);
   };
+
+  const deleteCourse = (courseId: string) => {
+    setCourses(courses.filter((course) => course._id !== courseId));
+  };
+
+  const updateCourse = () => {
+    setCourses(
+      courses.map((c) => {
+        if (c._id === course._id) {
+          return course;
+        } else {
+          return c;
+        }
+      })
+    );
+  };
+
 
 
   return (
@@ -23,6 +40,10 @@ export default function Dashboard() {
           <button className="btn btn-primary float-end"
                   id="wd-add-new-course-click"
                   onClick={addNewCourse} > Add </button>
+                    <button className="btn btn-warning float-end me-2"
+                onClick={updateCourse} id="wd-update-course-click">
+          Update
+        </button>
       </h5><br />
       <input defaultValue={course.name} className="form-control mb-2"
              onChange={(e) => setCourse({ ...course, name: e.target.value }) } />
@@ -48,6 +69,24 @@ export default function Dashboard() {
                     <p className="wd-dashboard-course-title card-text overflow-y-hidden" style={{ maxHeight: 100 }}>
                       {course.description} </p>
                     <button className="btn btn-primary"> Go </button>
+                    
+                    <button onClick={(event) => {
+                      event.preventDefault();
+                      deleteCourse(course._id);
+                    }} className="btn btn-danger float-end"
+                    id="wd-delete-course-click">
+                    Delete
+            </button>
+            
+                          <button id="wd-edit-course-click"
+                onClick={(event) => {
+                  event.preventDefault();
+                  setCourse(course);
+                }}
+                className="btn btn-warning me-2 float-end" >
+                Edit
+              </button>
+
                   </div>
                 </Link>
               </div>
