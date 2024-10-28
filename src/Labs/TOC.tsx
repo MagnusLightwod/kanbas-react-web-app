695b04edcedd02a24e5d5f91cef11caf6fe2d4d9
Maggy <chrismegaman@live.com>
2024-10-28 16:57:11 -0400

    fixed link to github

--- src/Labs/TOC.tsx
+++ src/Labs/TOC.tsx
@@ -42,7 +42,7 @@ export default function TOC() {
         </a>
       </li>
       <li className="nav-item">
-        <a id="wd-github" href="https://github.com/MagnusLightwod/kanbas-react-web-app/tree/a3" className="nav-link">
+        <a id="wd-github" href="https://github.com/MagnusLightwod/kanbas-react-web-app/tree/a4" className="nav-link">
           My GitHub
         </a>
       </li>
