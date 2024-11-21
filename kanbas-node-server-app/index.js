34a6d9f7ec8638a38b8b0103a6f80759d3ae47b9
Maggy <chrismegaman@live.com>
2024-11-21 16:03:13 -0500

    changed app.tsx

--- kanbas-node-server-app/index.js
+++ kanbas-node-server-app/index.js
@@ -30,7 +30,9 @@ app.use(cors({
       sameSite: 'lax' // Please use your own value based on requirements.
     }
   }
-
+app.get('/', (req, res) => {
+  res.redirect('/Kanbas/Labs/Home'); // Adjust the path as needed for your labs homepage
+});
 //   if (process.env.NODE_ENV !== "development") {
 //     sessionOptions.proxy = true;
 //     sessionOptions.cookie.sameSite = 'none';
