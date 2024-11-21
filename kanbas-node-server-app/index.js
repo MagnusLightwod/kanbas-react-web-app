8b28eacd0f863f854b1277340d22e467502634cc
Maggy <chrismegaman@live.com>
2024-11-21 12:32:15 -0500

    changes

--- kanbas-node-server-app/index.js
+++ kanbas-node-server-app/index.js
@@ -2,13 +2,15 @@ import express from 'express';
 import Hello from "./Hello.js";
 import Lab5 from "./Lab5/index.js";
 import cors from "cors";
-import UserRoutes from "../Kanbas/Users/routes.js";
 import CourseRoutes from "../Kanbas/Courses/routes.js";
+import UserRoutes from "../Kanbas/Users/routes.js";
 import ModuleRoutes from "../Kanbas/Modules/routes.js";
 import AssignmentRoutes from '../Kanbas/Assignments/routes.js';
 import session from "express-session";
 import "dotenv/config";
 import EnrollmentsRoutes from '../Kanbas/Enrollments/routes.js';
+
+
 const app = express();
 
 app.use(cors({
@@ -36,11 +38,10 @@ app.use(cors({
 //     sessionOptions.cookie.domain = process.env.NODE_SERVER_DOMAIN;
 // }
 app.use(session(sessionOptions));
-  
-
 app.use(express.json()); // allows for json and http request bodys to send data securely 
-UserRoutes(app);
+
 ModuleRoutes(app);
+UserRoutes(app);
 EnrollmentsRoutes(app);
 CourseRoutes(app); // course available only using our api. 
 AssignmentRoutes(app);
