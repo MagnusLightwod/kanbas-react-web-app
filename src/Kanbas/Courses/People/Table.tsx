db6b76e328113bd8a88e93e916527608be9d93bb
Maggy <chrismegaman@live.com>
2024-11-24 16:56:46 -0500

    users screen fixed, filters added

--- src/Kanbas/Courses/People/Table.tsx
+++ src/Kanbas/Courses/People/Table.tsx
@@ -7,6 +7,9 @@ export default function PeopleTable({ users = []}: {users?: any[]}) {
 
   return (
     <div id="wd-people-table">
+      <thead>
+          <tr> <th>Name</th> <th>Login ID</th><th>Section</th><th>Role</th><th>Last Activity</th> <th>Total Activity</th></tr>
+        </thead>
       <tbody>
   {users.map((user: any) => (
       <tr key={user._id}>
--- src/Kanbas/Navigation.tsx
+++ src/Kanbas/Navigation.tsx
@@ -15,7 +15,6 @@ export default function KanbasNavigation() {
     { label: "Calendar",  path: "/Kanbas/Calendar",  icon: IoCalendarOutline },
     { label: "Inbox",     path: "/Kanbas/Inbox",     icon: FaInbox },
     { label: "Labs",      path: "/Labs",             icon: LiaCogSolid },
-    // { label: "Users",     path: "/Kanbas/Account/users", icon: FaPeopleGroup}
   ];
 
   console.log("About to get current user");
