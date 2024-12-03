dc4ab08b39f8740144ace1a963b874c35d33e17c
Maggy <chrismegaman@live.com>
2024-12-03 17:51:34 -0500

    test

--- kanbas-node-server-app/index.js
+++ kanbas-node-server-app/index.js
@@ -24,35 +24,35 @@ app.use(cors({
   })); 
     
   // these options are for online deploy
-//   const sessionOptions = {
+  const sessionOptions = {
 
-
-//     secret: process.env.SESSION_SECRET || 'super secret session phrase', // Use a strong secret key 
-//     resave: false,
-//     saveUninitialized: true,
-//     cookie: {
-//         domain: process.env.NODE_SERVER_DOMAIN.replace('https://', ''),
-//         maxAge: 5000 * 60, // 5 minutes
-//         httpOnly: true,
-//         secure: process.env.NODE_ENV !== "development", // Use secure cookie in production (over HTTPS)
-//         sameSite: process.env.NODE_ENV === "development" ? 'lax' : 'none', // Use 'none' in production for cross-site cookies
-//     },
-// };
+    
+    secret: process.env.SESSION_SECRET || 'super secret session phrase', // Use a strong secret key 
+    resave: false,
+    saveUninitialized: true,
+    cookie: {
+        domain: process.env.NODE_SERVER_DOMAIN.replace('https://', ''),
+        maxAge: 5000 * 60, // 5 minutes
+        httpOnly: true,
+        secure: process.env.NODE_ENV !== "development", // Use secure cookie in production (over HTTPS)
+        sameSite: process.env.NODE_ENV === "development" ? 'lax' : 'none', // Use 'none' in production for cross-site cookies
+    },
+};
 
   // these options are for local deploy
-const sessionOptions = {
-    secret: 'sessionSecret', // Replace with a strong secret key
-    resave: false,
-    httpOnly: true,
-    saveUninitialized: true,
+// const sessionOptions = {
+//     secret: 'sessionSecret', // Replace with a strong secret key
+//     resave: false,
+//     httpOnly: true,
+//     saveUninitialized: true,
 
-    cookie: {
-      domain: 'localhost',
-      path: '/',
-      maxAge: 5000 * 60, //5 mnt
-      sameSite: 'lax' // Please use your own value based on requirements.
-    }
-  }
+//     cookie: {
+//       domain: 'localhost',
+//       path: '/',
+//       maxAge: 5000 * 60, //5 mnt
+//       sameSite: 'lax' // Please use your own value based on requirements.
+//     }
+//   }
 
 // Set proxy trust if in production, necessary when behind a reverse proxy like Render or Heroku
 if (process.env.NODE_ENV !== "development") {
